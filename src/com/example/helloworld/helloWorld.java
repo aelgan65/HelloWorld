4f37f8f3097f6b4cdf55ec9a7da142c653b71bd8
unknown <aelgan65@gmail.com>
2014-11-30 15:32:41 +1100

    initial checkin

---
+++ src/com/example/helloworld/helloWorld.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+/**
+ * Created by Elgan on 30/11/2014.
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world (blobby dog!!)");
+
+
+        System.out.println
+                ("hi hi" +
+                        " more" +
+                        " !");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i is " + i);
+        }
+    }
+
+}
